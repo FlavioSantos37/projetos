1d14ca35830abc64b8de75b17002f938a2b81575
FlavioSantos37 <desenvolvedorbr37@gmail.com>
2024-10-11 05:29:29 -0300

    deste

---
+++ javascript.js
@@ -0,0 +1,22 @@
+const receb = document.querySelector('.btn')
+const en = document.querySelector('.enviar')
+ const d = document.querySelector('.disponivel')
+
+
+ function convertValues(){
+
+
+    window.open("janela.html","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=400")
+	
+}
+
+
+receb.addEventListener("click",convertValues)
+
+
+function env(){
+    d.innerHTML='disponivel'
+    
+}
+
+ en.addEventListener('click',env)
