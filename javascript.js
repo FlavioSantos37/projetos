57f2e0c896980d6c9bf44619ac84085517267b28
Flavio <desenvolvedorbr37@gmail.com>
2024-10-13 19:30:39 -0300

    1310

--- javascript.js
+++ javascript.js
@@ -1,21 +1,19 @@
-const receb = document.querySelector('.btn')
-const en = document.querySelector('.enviar')
+ const receb = document.querySelector('.btn')
+ const en = document.querySelector('.enviar')
  const d = document.querySelector('.disponivel')
 
 
  function convertValues(){
-
-
     window.open("janela.html","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=400")
-	
 }
-
 
 receb.addEventListener("click",convertValues)
 
 
+
 function env(){
-    d.innerHTML='disponivel'
+    
+    d.innerHTML='indisponivel'
     
 }
 
